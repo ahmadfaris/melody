2e158b190be15510ed5716b91f514494a27c602b
ahmadfaris <faris91@gmail.com>
2015-08-03 23:17:22 +0800

    Add DB, food, drinks, and alter table restaurant to implement images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728164556) do
+ActiveRecord::Schema.define(version: 20150803151432) do
+
+  create_table "drinks", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +33,12 @@ ActiveRecord::Schema.define(version: 20150728164556) do
     t.string   "category"
     t.decimal  "lat"
     t.decimal  "lng"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
